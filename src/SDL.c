c95641f9cbc162211bde854c10cafe083ccd669d
Anonymous Maarten <anonymous.maarten@gmail.com>
2023-03-24 22:49:05 +0100

    SDL: use SDL_PLATFORM_BSDI instead of __BSDI__

--- src/SDL.c
+++ src/SDL.c
@@ -552,7 +552,7 @@ SDL_GetPlatform(void)
     return "AIX";
 #elif defined(__ANDROID__)
     return "Android";
-#elif defined(__BSDI__)
+#elif defined(SDL_PLATFORM_BSDI)
     return "BSDI";
 #elif defined(__DREAMCAST__)
     return "Dreamcast";
